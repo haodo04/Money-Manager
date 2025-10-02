b9bc2612b0fae444ea47d9c46b0b3fd03c7fc010
haodo04 <22130077@st.hcmuaf.edu.vn>
2025-10-02 23:19:22 +0700

    category icon picker

--- frontend/src/components/AddCategoryForm.jsx
+++ frontend/src/components/AddCategoryForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Input from "./Input";
+import EmojiPickerPopup from "./EmojiPickerPopup";
 
 const AddCategoryForm = () => {
     const [category, setCategory] = useState({
@@ -18,9 +19,14 @@ const AddCategoryForm = () => {
     }
     return (
         <div className="p-4">
+            <EmojiPickerPopup
+                icon={category.icon}
+                onSelect={(selectIcon) => handleChange("icon", selectIcon)}
+            />
+
             <Input
                 value={category.name}
-                onChange={(e) => handleChange("name", target.value)}
+                onChange={(target) => handleChange("name", target.value)}
                 label = "Category Name"
                 placeholder="e.g., Freelance, Salary, Groceries"
                 type="text"
