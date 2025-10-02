6e0382ad58f2e8d868a79d324bdee7bbb7dcfcc1
haodo04 <22130077@st.hcmuaf.edu.vn>
2025-10-02 21:13:50 +0700

    use hook function to fetch user info

--- frontend/src/util/apiEndpoints.jsx
+++ frontend/src/util/apiEndpoints.jsx
@@ -4,6 +4,7 @@ const CLOUDINARY_CLOUD_NAME = "dz32nqnp3"
 const API_ENDPOINTS = {
     LOGIN: "/login",
     REGISTER: "/register",
+    GET_USER_INFO: "/profile",
     UPLOAD_IMAGE: `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`
 }
 
