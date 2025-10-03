05cfd197f9fffc11b8dbc3ef83f087de749bbc8b
haodo04 <22130077@st.hcmuaf.edu.vn>
2025-10-03 09:14:20 +0700

    api get incomes and income page design

--- frontend/src/pages/Income.jsx
+++ frontend/src/pages/Income.jsx
@@ -1,14 +1,64 @@
-import React from 'react'
-import Dashboard from '../components/Dashboard'
-import { useUser } from '../hooks/useUser';
+import { useEffect, useState } from "react";
+import Dashboard from "../components/Dashboard";
+import { useUser } from "../hooks/useUser";
+import axiosConfig from "../util/axiosConfig";
+import API_ENDPOINTS from "../util/apiEndpoints";
+import toast from "react-hot-toast";
+import IncomeList from "../components/IncomeList";
 
 const Income = () => {
   useUser();
+  const [incomeData, setIncomeData] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState([]);
+
+  const [openAddIncomeModal, setOpenAddIncomeModal] = useState(false);
+  const [openDeleteAlert, setOpenDeleteAlert] = useState({
+    show: false,
+    data: null,
+  });
+
+  const fetchIncomeDetails = async () => {
+    if (loading) return;
+
+    setLoading(true);
+
+    try {
+      const response = await axiosConfig.get(API_ENDPOINTS.GET_ALL_INCOMES);
+      if (response.status === 200) {
+        console.log("Income list", response.data);
+        setIncomeData(response.data);
+      }
+    } catch (error) {
+      console.log("Failed to fetch income details", error);
+      toast.error(
+        error.response?.data?.message || "Failed to fetch income data"
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchIncomeDetails();
+  }, []);
+
   return (
     <Dashboard activeMenu="Income">
-      This is income page
-    </Dashboard>
-  )
-}
+      <div className="my-5 mx-auto">
+        <div className="grid grid-cols-1 gap-6">
+          <div>
+            {/* overview for income with line char */}
 
-export default Income
+            <IncomeList
+              transactions={incomeData}
+              onDelete={(id) => console.log("deleting the income", id)}
+            />
+          </div>
+        </div>
+      </div>
+    </Dashboard>
+  );
+};
+
+export default Income;
--- frontend/src/util/apiEndpoints.jsx
+++ frontend/src/util/apiEndpoints.jsx
@@ -8,6 +8,7 @@ const API_ENDPOINTS = {
     GET_ALL_CATEGORIES: "/categories",
     ADD_CATEGORY: "/categories",
     UPDATE_CATEGORY: (categoryId) => `/categories/${categoryId}`,
+    GET_ALL_INCOMES: "/incomes",
     UPLOAD_IMAGE: `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`
 }
 
