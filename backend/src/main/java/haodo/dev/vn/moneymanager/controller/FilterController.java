31bf1d64e773ee1ae30cfbd752be9c23868a6378
haodo04 <22130077@st.hcmuaf.edu.vn>
2025-10-07 21:32:09 +0700

    dashboard home page ui

--- backend/src/main/java/haodo/dev/vn/moneymanager/controller/FilterController.java
+++ backend/src/main/java/haodo/dev/vn/moneymanager/controller/FilterController.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/filter")
+@RequestMapping("/filter") 
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class FilterController {
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
 import Dashboard from '../components/Dashboard'
 import { useUser } from '../hooks/useUser'
+import InfoCard from '../components/InfoCard';
+import { WalletCards } from 'lucide-react';
+import { addThousandsSeparator } from '../util/util';
 
 const Home = () => {
   useUser();
   return (
     <div>
       <Dashboard activeMenu="Dashboard">
-        This is home page
+        <div className='my-5 mx-auto'>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {/* Display the cards */}
+            <InfoCard
+              icon={<WalletCards/>}
+              label="Total Balance"
+              value={addThousandsSeparator(100000.00)}
+              color="bg-purple-800"
+            />
+
+            <InfoCard
+              icon={<WalletCards/>}
+              label="Total Income"
+              value={addThousandsSeparator(200000.00)}
+              color="bg-green-800"
+            />
+
+            <InfoCard
+              icon={<WalletCards/>}
+              label="Total Expense"
+              value={addThousandsSeparator(50000.00)}
+              color="bg-red-800"
+            />
+          </div>
+          <div className='grid-cols-1 md:grid-cols-2 gap-6 mt-6'>
+            {/* Recent transactions */}
+
+            {/* finance overview chart */}
+
+            {/* Expense transactions */}
+
+            {/* Income transactions */}
+          </div>
+        </div>
       </Dashboard>
     </div>
   )
