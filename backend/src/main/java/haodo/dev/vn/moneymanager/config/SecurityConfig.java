b4a7d2196bda57f4cf6a5188b2f8b4b5acc1c072
haodo04 <22130077@st.hcmuaf.edu.vn>
2025-10-09 23:56:19 +0700

    fix response 403 for api expense

--- backend/src/main/java/haodo/dev/vn/moneymanager/config/SecurityConfig.java
+++ backend/src/main/java/haodo/dev/vn/moneymanager/config/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/status"
                 , "/health", "/register", "/activation", "/login")
                         .permitAll().anyRequest().authenticated())
+                .requestMatchers(HttpMethod.POST, "/api/expense/**").hasAnyRole("ADMIN","STAFF","USER")
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
                 .formLogin(AbstractHttpConfigurer::disable);
